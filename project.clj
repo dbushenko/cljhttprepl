afedc7c5c0100fc786bf0dc9cb73a4517b0840de
dbushenko <d.bushenko@gmail.com>
2016-06-23 12:21:12 +0300

    Initial commit

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject cljhttprepl "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.5.1"]
+                 [ring/ring-jetty-adapter "1.5.0"]
+                 [ring/ring-defaults "0.2.1"]]
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler cljhttprepl.handler/app}
+  :main cljhttprepl.handler
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]]}})
---
+++ src/cljhttprepl/handler.clj
@@ -0,0 +1,39 @@
+(ns cljhttprepl.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [clojure.string :as str]
+            [ring.adapter.jetty :as jetty]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]])
+  (:gen-class)
+)
+
+(def ^:dynamic *token* (atom nil))
+
+(defn run-command [body query]
+    (try
+        (let [token (get (apply hash-map (str/split query #"=")) "token")]
+            (if (and (not (nil? token)) (= token @*token*))
+                (str (eval (read-string (slurp body))))
+                "Wrong auth token!"))
+        (catch Exception e
+            "Wrong query string!"))
+)
+
+(defroutes repl-routes
+  (POST "/repl" {body :body, query :query-string}
+      (if-not (empty? @*token*)
+          (run-command body query)
+          "No auth token supplied!"))
+)
+
+(defn set-repl-token! [ token ]
+    (reset! *token* token)
+)
+
+(def app
+  (wrap-defaults repl-routes site-defaults))
+
+(defn -main [& params]
+    (set-repl-token! "456")
+    (jetty/run-jetty repl-routes {:port 3000 :join? false})
+)    
